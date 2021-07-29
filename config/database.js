6cdf59bf1c443372ae1b1dba9864712cc62dbbbf
Marcos <mrsilvatdas@gmail.com>
2021-07-29 18:57:51 -0300

    Teste da maneira antiga

--- config/database.js
+++ config/database.js
@@ -32,22 +32,22 @@ module.exports = {
     debug: Env.get('DB_DEBUG', false)
   },
 
-  // pg: {
-  //   client: 'pg',
-  //   connection: {
-  //     host: Env.get('DB_HOST', 'localhost'),
-  //     port: Env.get('DB_PORT', ''),
-  //     user: Env.get('DB_USER', 'root'),
-  //     password: Env.get('DB_PASSWORD', ''),
-  //     database: Env.get('DB_DATABASE', 'adonis')
-  //   },
-  //   debug: Env.get('DB_DEBUG', false)
-  // }
   pg: {
     client: 'pg',
-    connection: Env.get('DATABASE_URL', ''),
-    // 'postgres://qjoulbrztsremz:47d96661586186d7aac17f4c6537dda11155588803c70e57fbf9916c00e0f621@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d5l84cbeq9qc3p',
-    // Env.get('DATABASE_URL', '') 
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
     debug: Env.get('DB_DEBUG', false)
   }
+  // pg: {
+  //   client: 'pg',
+  //   connection: Env.get('DATABASE_URL', ''),
+  //   // 'postgres://qjoulbrztsremz:47d96661586186d7aac17f4c6537dda11155588803c70e57fbf9916c00e0f621@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d5l84cbeq9qc3p',
+  //   // Env.get('DATABASE_URL', '') 
+  //   debug: Env.get('DB_DEBUG', false)
+  // }
 }
