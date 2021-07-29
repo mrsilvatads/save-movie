cd077e5c3e05ebeba1a739d24bac54d1169df88d
Marcos <mrsilvatdas@gmail.com>
2021-07-29 20:50:36 -0300

    Comentando as outras conexões

--- config/database.js
+++ config/database.js
@@ -11,29 +11,17 @@ module.exports = {
   connection: Env.get('DB_CONNECTION', 'pg'),
 
  
-  sqlite: {
-    client: 'sqlite3',
-    connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
-    },
-    useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
-  },
+  // sqlite: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+  //   },
+  //   useNullAsDefault: true,
+  //   debug: Env.get('DB_DEBUG', false)
+  // },
 
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
-  },
-
-  // pg: {
-  //   client: 'pg',
+  // mysql: {
+  //   client: 'mysql',
   //   connection: {
   //     host: Env.get('DB_HOST', 'localhost'),
   //     port: Env.get('DB_PORT', ''),
@@ -42,12 +30,24 @@ module.exports = {
   //     database: Env.get('DB_DATABASE', 'adonis')
   //   },
   //   debug: Env.get('DB_DEBUG', false)
-  // }
+  // },
+
   pg: {
     client: 'pg',
-    connection: 'postgres://ciwmvvyqkpeuhr:dbedfef12152f385d863dd9f04bb631827adcbd5879bec869f5628886a765032@ec2-52-0-67-144.compute-1.amazonaws.com:5432/d7plsckal01i2e?sslmode=require',
-    // 'postgres://qjoulbrztsremz:47d96661586186d7aac17f4c6537dda11155588803c70e57fbf9916c00e0f621@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d5l84cbeq9qc3p',
-    // Env.get('DATABASE_URL', '') 
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
     debug: Env.get('DB_DEBUG', false)
   }
+  // pg: {
+  //   client: 'pg',
+  //   connection: 'postgres://ciwmvvyqkpeuhr:dbedfef12152f385d863dd9f04bb631827adcbd5879bec869f5628886a765032@ec2-52-0-67-144.compute-1.amazonaws.com:5432/d7plsckal01i2e?sslmode=require',
+  //   // 'postgres://qjoulbrztsremz:47d96661586186d7aac17f4c6537dda11155588803c70e57fbf9916c00e0f621@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d5l84cbeq9qc3p',
+  //   // Env.get('DATABASE_URL', '') 
+  //   debug: Env.get('DB_DEBUG', false)
+  // }
 }
